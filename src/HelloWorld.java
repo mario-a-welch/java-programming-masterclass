6692ee685226907cd2c4d1b665a0c22834887535
mario welch <mario.welch01@gmail.com>
2022-08-12 19:16:01 -0500

    Variables & Expressions

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,22 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello, Mario!");
+
+//        Variables Lec
+        int myFirstNumber = 7;
+        System.out.println(myFirstNumber);
+//        Java Operators (+, -, *, / )
+
+//        Expressions Lec
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+
     }
 }
