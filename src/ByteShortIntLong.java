c5993e69a0e42bf4b5a73ec371235555a75396e1
mario welch <mario.welch01@gmail.com>
2022-08-12 20:14:44 -0500

    primitive lec

---
+++ src/ByteShortIntLong.java
@@ -0,0 +1,17 @@
+public class ByteShortIntLong {
+    public static void main(String[] args) {
+
+//        Primitive Type Lec
+        int myValue = 1000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted Maximum Value = " + (myMaxIntValue + 1));
+        System.out.println("Busted Minimum Value = " + (myMinIntValue - 1));
+
+
+    }
+}
