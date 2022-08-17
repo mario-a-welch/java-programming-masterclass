16019c2c47083c64826809e2b220e41d783f38cc
mario welch <mario.welch01@gmail.com>
2022-08-17 14:30:23 -0500

    char and boolean

---
+++ src/CharBool.java
@@ -0,0 +1,18 @@
+public class CharBool {
+    public static void main(String[] args){
+
+//        Char and Boolean Lec
+        char myChar = 'D';
+        char myUniCodeChar = '\u0044'; //From unicode-table.com
+        char myCopyrightChar = '\u00A9';
+
+        System.out.println(myChar);
+        System.out.println(myUniCodeChar);
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBooleanVal = true;
+        boolean myFalseBooleanVal = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
