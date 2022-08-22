9d1c8bf5b76517c21f601fd7a5ea97d994bc2803
mario welch <mario.welch01@gmail.com>
2022-08-21 22:04:19 -0500

    logical and operator

--- src/Operators.java
+++ src/Operators.java
@@ -39,5 +39,15 @@ public class Operators {
         if (isAlien == false){
             System.out.println("It is not a Alien");
         }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score");
+        }
+
+        int secondTopScore = 81;
+        if (topScore > secondTopScore && topScore < 100){
+            System.out.println("Greater than second top score and less than 100");
+        }
     }
 }
