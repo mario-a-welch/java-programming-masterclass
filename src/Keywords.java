aa7d18f73749e3f456ce410f97dbec4d054a5a4d
mario welch <mario.welch01@gmail.com>
2022-08-25 19:50:46 -0500

    keywords and expressions

---
+++ src/Keywords.java
@@ -0,0 +1,12 @@
+public class Keywords {
+    public static void main(String[] args) {
+//        Keywords - double, int, if
+//        Expressions are usually what follows keywords
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50){
+            System.out.println("This is an expression");
+        }
+    }
+}
