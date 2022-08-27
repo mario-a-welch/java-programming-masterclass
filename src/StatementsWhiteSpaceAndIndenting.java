2288e995e40c84fbb0f641fc0499128b21d1c772
mario welch <mario.welch01@gmail.com>
2022-08-26 22:13:38 -0500

    statements

---
+++ src/StatementsWhiteSpaceAndIndenting.java
@@ -0,0 +1,19 @@
+public class StatementsWhiteSpaceAndIndenting {
+    public static void main(String[] args){
+        //Expression - myVariable = 50;
+        //Statement - int myVariable = 50;
+        //Statement - myVariable++;
+
+        //Java does get rid of whitespace when running the code,
+        //but it is best practice to include spaces for readability
+
+        //Indenting is used to specify code blocks for logical flow and readability
+
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+
+    }
+}
