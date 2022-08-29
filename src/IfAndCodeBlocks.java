914b7812b130f94d1f5aaf320ce5ea81e97de0e5
mario welch <mario.welch01@gmail.com>
2022-08-28 22:30:19 -0500

    methods part 1

--- src/IfAndCodeBlocks.java
+++ src/IfAndCodeBlocks.java
@@ -16,6 +16,7 @@ public class IfAndCodeBlocks {
 
         if(gameOver == true) {
             int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
 //            A variable created in the code block cannot be accessed outside the code block - outside scope
 //            You can access variables that are created outside the code block
