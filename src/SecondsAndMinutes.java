c1c153bfdb0ab90ab2c009e14ecb7bf758eefb8c
mario welch <mario.welch01@gmail.com>
2022-09-06 08:20:21 -0500

    seconds and minutes bonus challenge

--- src/SecondsAndMinutes.java
+++ src/SecondsAndMinutes.java
@@ -1,18 +1,39 @@
 public class SecondsAndMinutes {
 
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
+
     public static String getDurationString(long minutes, long seconds){
         if((minutes < 0) || (seconds < 0) || (seconds > 59)){
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         long hours = minutes / 60;
         long remainingMinutes = minutes % 60;
-        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+
+        //More optimal ways to complete following code, but used what was currently taught
+        String hoursString =  hours + "h";
+        if(hours < 10){
+            hoursString = "0" + hoursString;
+        }
+
+        //More optimal ways to complete following code, but used what was currently taught
+        String minutesString =  remainingMinutes + "m";
+        if(hours < 10){
+            minutesString = "0" + minutesString;
+        }
+
+        //More optimal ways to complete following code, but used what was currently taught
+        String secondsString =  seconds + "s";
+        if(hours < 10){
+            secondsString = "0" + secondsString;
+        }
+
+        return hoursString + " " + minutesString + " " + secondsString;
     }
 
     public static String getDurationString(long seconds){
         if(seconds < 0){
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         long minutes = seconds / 60;
