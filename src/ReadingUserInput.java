5c42bf2ba9135e6b5f44a2fbb4ea0c0c56881a73
mario welch <mario.welch01@gmail.com>
2022-09-21 13:08:21 -0500

    verify user input

--- src/ReadingUserInput.java
+++ src/ReadingUserInput.java
@@ -8,15 +8,26 @@ public class ReadingUserInput {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your year of birth:");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine(); // needed to handle next line enter key
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        int age = 2022 - yearOfBirth;
+        boolean hasNextInt = scanner.hasNextInt(); // checks to see if next user input is an int
 
-        System.out.println("Your name is " + name);
-        System.out.println("Your age is " + age);
+        if(hasNextInt){
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // needed to handle next line enter key
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2022 - yearOfBirth;
+
+            if(age >= 0 && age <= 100){
+                System.out.println("Your name is " + name + ", you are " + age + " years old.");
+            } else{
+                System.out.println("Invalid age");
+            }
+        } else{
+            System.out.println("Unable to parse year of birth");
+        }
+
 
         scanner.close();
 
