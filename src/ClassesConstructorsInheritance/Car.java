4073aef814477f56d8224d0185163f0444f89a67
mario welch <mario.welch01@gmail.com>
2022-09-22 11:19:29 -0500

    classes part 1

---
+++ src/ClassesConstructorsInheritance/Car.java
@@ -0,0 +1,10 @@
+package ClassesConstructorsInheritance;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+}
---
+++ src/ClassesConstructorsInheritance/Main.java
@@ -0,0 +1,11 @@
+package ClassesConstructorsInheritance;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car ferrari = new Car();
+        Car toyota = new Car();
+
+
+    }
+}
